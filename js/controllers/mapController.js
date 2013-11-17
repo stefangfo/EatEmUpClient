2fe664a7f7ac53d5742cea3b4db098bb0697bf2d
Stefan Gföllner <sgfoellner@gmx.at>
2013-11-17 23:04:25 +0100

    Map

--- js/controllers/mapController.js
+++ js/controllers/mapController.js
@@ -3,6 +3,9 @@ var mapController = function(){
 var map = null;
 var goodies = [];	
 var playerAvatars = [];
+var avatarImages = {red: null, blue: null};
+var goodieImages = {standard: null, big: null, special: null};
+var specialActionImages = {doublePoints: null, invincible: null};
 var playerAvatarWidth = 72;
 var playerAvatarHeight = 95;
 var positioningInterval;
@@ -15,13 +18,9 @@ this.initMap = function(mapElement) {
 	    
 	    if(!isInitialized(mapElement)) {
 	    	 map = new google.maps.Map(document.getElementById(mapElement), mapOptions);
-			 this.initMarkers(map);
-			//getLocation();
-		}else{
-			//map already exists
-			this.initMarkers(map);
-			//getLocation();
 		}
+		this.initMarkers();
+		//getLocation();
 }
 
 function getLocation() {
@@ -51,193 +50,236 @@ this.stopPositioning = function() {
 }
 
 
-this.initMarkers = function(map){
-	
-	var standardImg = new google.maps.MarkerImage("img/standard.png",
-												    null, /* size is determined at runtime */
-												    null, /* origin is 0,0 */
-												    null, /* anchor is bottom center of the scaled image */
-												    new google.maps.Size(10, 10));
-												    
-	var bigImg = new google.maps.MarkerImage("img/big.png",
-												    null, /* size is determined at runtime */
-												    null, /* origin is 0,0 */
-												    new google.maps.Point(0, 10), /* anchor is bottom center of the scaled image */
-												    new google.maps.Size(20, 20));	
-	
-	
-	var standardGoodie1 = new google.maps.Marker({
-    	position: new google.maps.LatLng(48.337066, 14.318477),
-		map: map,
-		icon: standardImg
-	});
-	var standardGoodie2 = new google.maps.Marker({
-    	position: new google.maps.LatLng(48.337191, 14.318498),
-		map: map,
-		icon: standardImg
-	});
-	var standardGoodie3 = new google.maps.Marker({
-    	position: new google.maps.LatLng(48.337191, 14.318498),
-		map: map,
-		icon: standardImg
-	});
-	var standardGoodie4 = new google.maps.Marker({
-    	position: new google.maps.LatLng(48.3369554, 14.318458),
-		map: map,
-		icon: standardImg
+this.initMarkers = function(){
+	//load images from server
+	loadImages(function() {
+		//init avatar markers
+		var playerInfo = [{username: "Stefan Gföllner", userid: "1690871472", position: {latitude: 48.337428, longitude: 14.319928}}, {username: "Michael Hartlauer", userid: "1690871472", 								position: {latitude: 48.337328, longitude: 14.321237}}];
+		initAvatarMarkers(playerInfo);
+		
+		//init goodie markers
+		var goodieInfo = [{latitude: 48.337066, longitude: 14.318477, specialAction: null, points: 50}, {latitude: 48.337191, longitude: 14.318498, specialAction: null, points: 25}];
+		initGoodieMarkers(goodieInfo);
 	});
 	
-	var bigGoodie1 = new google.maps.Marker({
-    	position: new google.maps.LatLng(48.338027, 14.319364),
-		map: map,
-		icon: bigImg
-	});
-	var bigGoodie2 = new google.maps.Marker({
-    	position: new google.maps.LatLng(48.337952, 14.320378),
-		map: map,
-		icon: bigImg
-	});
-	
-	goodies.push(standardGoodie1);
-	goodies.push(standardGoodie2);
-	goodies.push(standardGoodie3);
-	goodies.push(standardGoodie4);
-	goodies.push(bigGoodie1);
-	goodies.push(bigGoodie2);
-	
-	createPlayerAvatar("red", "stefan", 200, function(pngURL, canvas) {
-		var player1 = new google.maps.Marker({
-	    	position: new google.maps.LatLng(48.337428, 14.319928),
-			map: map,
-			icon: pngURL
-		});
+
+}
+
+function initAvatarMarkers(playerInfo) {
+	for (var i=0; i<playerInfo.length; i++) {
+		var player = playerInfo[i];
+		//create avatar
+		createAvatar("red", player);	
+   }
+}
+
+function createAvatar(team, player) {
+	createPlayerAvatar("red", player.userid, player.username, 0, function(pngURL, canvas) {
+		//console.log(i);
+		var playerMarker = new google.maps.Marker({
+			    	position: new google.maps.LatLng(player.position.latitude, player.position.longitude),
+					map: map,
+					icon: pngURL
+		});		
+			
 		//callout
 		var infoWindowOpts = {
-		  content: "stefan"
-		};
-
-		var infoWindow = new google.maps.InfoWindow(infoWindowOpts);
-		google.maps.event.addListener(player1, 'click', function() {
-		  infoWindow.open(map, player1);
-		});
-		
-		playerAvatars.push({userID: "", username: "stefan", marker: player1, canvas: canvas});	
-		var amount = 0;
-		setInterval(function() {
-			amount = amount + 50;
-			redrawPlayerAvatar("stefan", amount, "invincible");
-		
-		}, 2000);
-		
-	});
+			  content: player.username
+		};		
 	
-	createPlayerAvatar("red", "markus", 300, function(pngURL, canvas) {
-		var redPlayer = new google.maps.Marker({
-	    	position: new google.maps.LatLng(48.336718, 14.319592),
-			map: map,
-			icon: pngURL
-		});
-		
-		//callout
-		var infoWindowOpts = {
-		  content: "markus"
-		};
-
 		var infoWindow = new google.maps.InfoWindow(infoWindowOpts);
-		google.maps.event.addListener(redPlayer, 'click', function() {
-		  infoWindow.open(map, redPlayer);
+		google.maps.event.addListener(playerMarker, 'click', function() {
+			infoWindow.open(map, playerMarker);
 		});
-		playerAvatars.push({userID: "", username: "markus", marker: redPlayer, canvas: canvas});
-	});
-
-	createPlayerAvatar("blue", "christian", 100, function(pngURL, canvas) {
-		var bluePlayer1 = new google.maps.Marker({
-	    	position: new google.maps.LatLng(48.336711, 14.319651),
-			map: map,
-			icon: pngURL
-		});
-		
-		//callout
-		var infoWindowOpts = {
-		  content: "christian"
-		};
-
-		var infoWindow = new google.maps.InfoWindow(infoWindowOpts);
-		google.maps.event.addListener(bluePlayer1, 'click', function() {
-		  infoWindow.open(map, bluePlayer1);
-		});
-		playerAvatars.push({userID: "", username: "christian", marker: bluePlayer1, canvas: canvas});
-	});	
+			
+		playerAvatars.push({userID: player.userid, username: player.username, specialAction: null, marker: playerMarker, canvas: canvas});	
 	
-	createPlayerAvatar("blue", "michael", 150, function(pngURL, canvas) {
-		var bluePlayer2 = new google.maps.Marker({
-	    	position: new google.maps.LatLng(48.337328, 14.321237),
-			map: map,
-			icon: pngURL
-		});
-		
-		//callout
-		var infoWindowOpts = {
-		  content: "michael"
-		};
-
-		var infoWindow = new google.maps.InfoWindow(infoWindowOpts);
-		google.maps.event.addListener(bluePlayer2, 'click', function() {
-		  infoWindow.open(map, bluePlayer2);
-		});
-		playerAvatars.push({userID: "", username: "michael", marker: bluePlayer2, canvas: canvas});
-		
-		var amount = 0;
-		setInterval(function() {
-			amount = amount + 50;
-			redrawPlayerAvatar("michael", amount, "doublePoints");
-		}, 3000);
+		//redraw once
+		//	redrawPlayerAvatar(player.userid, player.username, 50);
+		//	drawSpecialAction(player.userid, player.username, 100, "invincible");							
 	});	
 }
 
-function createPlayerAvatar(type, username, points, loadedCallback) {
+function initGoodieMarkers(goodieInfo) {
+	for (var i=0; i<goodieInfo.length; i++) {
+		var goodie = goodieInfo[i];
+		var img;
+		var goodieImage;
+		if (goodie.points === 25) {
+			goodieImage = goodieImages.standard;
+			markerImg = new google.maps.MarkerImage(goodieImage.src,
+												    null, 
+												    null, 
+												    null, 
+												    new google.maps.Size(10, 10));
+		}else if (goodie.points === 50) {
+			goodieImage = goodieImages.big;
+			markerImg = new google.maps.MarkerImage(goodieImage.src,
+												    null, 
+												    null, 
+												    null, 
+												    new google.maps.Size(20, 20));
+		}else if (goodie.specialAction) {
+			goodieImage = goodieImages.special;
+			markerImg = new google.maps.MarkerImage(goodieImage.src,
+												    null, 
+												    null, 
+												    null, 
+												    new google.maps.Size(20, 20));
+		}
+			
+		var goodie = new google.maps.Marker({
+    					position: new google.maps.LatLng(goodie.latitude, goodie.longitude),
+						map: map,
+						icon: markerImg
+		});
+		goodies.push(goodie);
+	}
+}
+
+
+function loadImages(callback) {
+	var loaded = 0;
+	var loadedCallback = function() {
+		loaded++;
+	    if (loaded === 3) {
+	    	callback();
+	    }
+	}
+	//load avatar images
+	loadAvatarImages(loadedCallback);
+	//load goodie images
+	loadGoodieImages(loadedCallback);
+	//load special action images
+	loadSpecialActionImages(loadedCallback);
+}
+
+function loadAvatarImages(callback) {
+	var imgSrcs = ["img/redAvatar.png", "img/blueAvatar.png"];
+	var loadCallback = function (imgs) {
+    	avatarImages.red = imgs[0];
+	    avatarImages.blue = imgs[1];
+	    callback();
+	};
+	
+	//error callback for image load
+	var errorCallback = function() {
+		console.log("Error in loading avatar images");
+	}
+	
+	//load images from server
+   loadImagesFromServer(imgSrcs, loadCallback, errorCallback);
+}
+
+function loadGoodieImages(callback) {
+	var imgSrcs = ["img/standard.png", "img/big.png", "img/special.png"];
+	var loadCallback = function (imgs) {
+    	goodieImages.standard = imgs[0];
+	    goodieImages.big = imgs[1];
+	    goodieImages.special = imgs[2];
+	    callback();
+	};
+	
+	//error callback for image load
+	var errorCallback = function() {
+		console.log("Error in loading goodie images");
+	}
+	
+	//load images from server
+   loadImagesFromServer(imgSrcs, loadCallback, errorCallback);
+}
+
+function loadSpecialActionImages(callback) {
+	var imgSrcs = ["img/invincible.png"];
+	var loadCallback = function (imgs) {
+    	specialActionImages.invincible = imgs[0];
+    	callback();
+	};
+	
+	//error callback for image load
+	var errorCallback = function() {
+		console.log("Error in loading specialAction images");
+	}
+	
+	//load images from server
+   loadImagesFromServer(imgSrcs, loadCallback, errorCallback);
+}
+
+function loadImagesFromServer(imgSrcs, loadedCallback, errorCallback) {
+	var loaded = 0;
+	var imgs = [];
+	var loadCallback = function () {
+    	loaded++;
+	    if (loaded === imgSrcs.length) {
+	    	loadedCallback(imgs);
+	    }
+	};
+	 
+	for (var i = 0; i < imgSrcs.length; i++) {
+	   imgs[i] = new Image();
+	   imgs[i].addEventListener('load', loadCallback, false);
+	   imgs[i].onerror = errorCallback;		
+	   imgs[i].src = imgSrcs[i];
+	}
+}
+
+
+function createPlayerAvatar(team, userID, username, points, loadedCallback) {
 	var canvas, context;
 	canvas = document.createElement("canvas");
     canvas.width = playerAvatarWidth;
     canvas.height = playerAvatarHeight;
     context = canvas.getContext("2d");
     
-    var imgSrcs;
-    if (type == "me") {
-    	imgSrcs = ["img/meAvatar.png", "img/"+username+".png"];
-    }else if (type == "blue") {
-	    imgSrcs = ["img/blueAvatar.png", "img/"+username+".png"];
-    }else {
-	    imgSrcs = ["img/redAvatar.png", "img/"+username+".png"];
-    }
-    
-    var loaded = 0;
-	var loadCallback = function () {
-    	loaded++;
-	    if (loaded == imgSrcs.length) {
-	    	drawPlayerAvatar(context, username, imgs[0], imgs[1], points, null);
-			loadedCallback(canvas.toDataURL(), {canvas: canvas, markerImg: imgs[0], userImg: imgs[1]});
-	    }
+    var imgSrcs = ["http://graph.facebook.com/"+userID+"/picture"];  
+    var loadCallback = function (imgs) {
+    	var avatarImg;
+    	if (team === "red"){
+	    	avatarImg = avatarImages.red;
+    	}else {
+	    	avatarImg = avatarImages.blue;
+    	}
+    	drawPlayerAvatar(context, username, avatarImg, null, points, null);
+    	loadedCallback(canvas.toDataURL(), {canvas: canvas, markerImg: avatarImg, userImg: null});
 	};
 	
+	//error callback for image load
 	var errorCallback = function() {
-	        drawPlayerAvatar(context, username, imgs[0], null, points, null);
-			loadedCallback(canvas.toDataURL(), {canvas: canvas, markerImg: imgs[0], userImg: null});
+		console.log("Error in loading specialAction images");
 	}
- 
-    var imgs = [];
-    for (var i = 0; i < imgSrcs.length; i++) {
-	    imgs[i] = new Image();
-	    imgs[i].addEventListener('load', loadCallback, false);
-	    imgs[i].onerror = errorCallback;		
-	    imgs[i].src = imgSrcs[i];
-	}
+	
+	//load images from server
+   loadImagesFromServer(imgSrcs, loadCallback, errorCallback);  
 }
 
-function drawPlayerAvatar(context, username, markerImg, userImg, points, specialAction) {
+function redrawPlayerAvatar(userID, username, points) {
+	var playerAvatar = getPlayerAvatarByUserID(userID);
+	//clear old canvas 
+	var canvas = playerAvatar.canvas.canvas;
+	var context = canvas.getContext("2d");
+	context.clearRect(0, 0, playerAvatarWidth, playerAvatarHeight);
+	
+	//draw new canvas
+	drawPlayerAvatar(context, username, playerAvatar.canvas.markerImg, playerAvatar.canvas.userImg, points, playerAvatar.specialAction);
+	//set new canvas to marker
+	playerAvatar.marker.setIcon(canvas.toDataURL());	
+}
+
+function drawSpecialAction(userID, username, points, specialAction) {
+	var playerAvatar = getPlayerAvatarByUserID(userID);
+	playerAvatar.specialAction = specialAction;
+	redrawPlayerAvatar(userID, username, points);
+}
+
+
+function removeSpecialAction(userID, username, points, specialAction) {
+	
+}
+
+function drawPlayerAvatar(context, username, markerImg, userImg, points, actionType) {
 	context.drawImage(markerImg, 0, 10, 62, 85);
 	context.fillStyle = "rgb(255,255,255)";
-	if (!(userImg == null)) {
+	if (userImg) {
 		context.drawImage(userImg, 5.0, 15, 52, 52);
 	}else {
 		context.textAlign = "center";
@@ -245,7 +287,7 @@ function drawPlayerAvatar(context, username, markerImg, userImg, points, special
 	 	if (username.length > 6){
 		 	username = username.substr(0, 6);
 	 	}
-		context.fillText(username, 31, 30); 
+		context.fillText(username, 31, 45); 
 	}
 	context.font = "10px sans-serif";
 	context.textAlign = "center";
@@ -253,8 +295,8 @@ function drawPlayerAvatar(context, username, markerImg, userImg, points, special
 	context.fillStyle = "rgb(0,0,0)";
 	context.lineWidth = 1;
 	context.strokeText(points+" Pkt.", 31, 75); 
-	
-	if (specialAction) {
+		
+	if (actionType) {
 		context.beginPath();
 		context.arc(55,16.5,15,0,2*Math.PI);
 		context.lineWidth = 3;
@@ -263,34 +305,24 @@ function drawPlayerAvatar(context, username, markerImg, userImg, points, special
 		context.fillStyle = 'white';
 		context.fill();
 		context.lineWidth = 1;
-		if (specialAction == "invincible") {
-			context.strokeText("UN", 55, 20);
+		if (actionType == "invincible") {
+			context.drawImage(specialActionImages.invincible, 44, 6, 22, 22);
 		}else {
 			context.strokeText("x2", 55, 20);
 		}
-	}
+	}		
 }
 
-//userID instead of username!!!!!
-function redrawPlayerAvatar(username, points, specialAction) {
-	var playerAvatar = getPlayerAvatarByUsername(username);
-	//clear old canvas 
-	var canvas = playerAvatar.canvas.canvas;
-	var context = canvas.getContext("2d");
-	context.clearRect(0, 0, playerAvatarWidth, playerAvatarHeight);
-	
-	//draw new canvas
-	drawPlayerAvatar(context, username, playerAvatar.canvas.markerImg, playerAvatar.canvas.userImg, points, specialAction)
-	
-	//set new canvas to marker
-	playerAvatar.marker.setIcon(canvas.toDataURL());
+
+function removeSpecialActionFromPlayer(userID) {
+	var playerAvatar = getPlayerAvatarByUserID(userID);
+	redr
 }
 
-//needs to be changed to userID!!!!!
-function getPlayerAvatarByUsername(username) {
+function getPlayerAvatarByUserID(userID) {
 	returnVal = null;
 	for (i=0; i<playerAvatars.length; i++){
-		if (playerAvatars[i].username == username) {
+		if (playerAvatars[i].userID == userID) {
 			returnVal = playerAvatars[i];
 			return returnVal;
 		}
